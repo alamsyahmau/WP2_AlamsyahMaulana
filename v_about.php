0fc37cf95435796e93798afe9d54132bcf386f04
alamsyahmau <12210901@bsi.ac.id>
2022-09-29 19:42:41 +0700

    menambahkan file v_about.php

---
+++ v_about.php
@@ -0,0 +1,22 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Alamsyah</li> 
+        <li>Nama Belakang : Maulana</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Letda Dadang Suprapto No.3 RT.003/005</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Tangerang</li> 
+    </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Pusreng</li> <li>Mobel Lejen</li> 
+    </ul> 
+</section>
