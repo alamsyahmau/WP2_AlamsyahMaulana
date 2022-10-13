e420cadc4fabd86f73f323e51ab84625a250c412
alamsyahmau <12210901@bsi.ac.id>
2022-10-13 14:02:57 +0700

    mengedit

---
+++ view-latihan2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+ <title>Latihan 2</title>
+</head>
+<body>
+ Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+ Panjang = <?= $nilai1; ?>
+ Lebar = <?= $nilai2; ?><br>
+ Hasil perhitungan keliling persegi panjang yaitu : <?=$keliling ?><br>
+ 
+ Hasil perhitungan luas persegi yaitu : <?=$luas?>
+</body>
+</html>
